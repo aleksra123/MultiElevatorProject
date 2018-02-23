4bbb1175d7a48e7b571d8aa5d4e8392bf107781d
[ASP] <aleksander.asp@outlook.com>
2018-02-23 15:18:11 +0100

    sjekke noe i driver

--- driver-go-master/main.go
+++ driver-go-master/main.go
@@ -8,27 +8,30 @@ func main(){
     numFloors := 4
 
     elevio.Init("localhost:15657", numFloors)
-    
+
     var d elevio.MotorDirection = elevio.MD_Up
     //elevio.SetMotorDirection(d)
-    
+
     drv_buttons := make(chan elevio.ButtonEvent)
     drv_floors  := make(chan int)
     drv_obstr   := make(chan bool)
-    drv_stop    := make(chan bool)    
-    
+    drv_stop    := make(chan bool)
+
     go elevio.PollButtons(drv_buttons)
     go elevio.PollFloorSensor(drv_floors)
     go elevio.PollObstructionSwitch(drv_obstr)
     go elevio.PollStopButton(drv_stop)
-    
-    
+
+    var v = <- drv_floors
+    if v != 0{
+      d = elevio.MD_Stop
+    }
     for {
         select {
         case a := <- drv_buttons:
             fmt.Printf("%+v\n", a)
             elevio.SetButtonLamp(a.Button, a.Floor, true)
-            
+
         case a := <- drv_floors:
             fmt.Printf("%+v\n", a)
             if a == numFloors-1 {
@@ -37,8 +40,8 @@ func main(){
                 d = elevio.MD_Up
             }
             elevio.SetMotorDirection(d)
-            
-            
+
+
         case a := <- drv_obstr:
             fmt.Printf("%+v\n", a)
             if a {
@@ -46,7 +49,7 @@ func main(){
             } else {
                 elevio.SetMotorDirection(d)
             }
-            
+
         case a := <- drv_stop:
             fmt.Printf("%+v\n", a)
             for f := 0; f < numFloors; f++ {
@@ -55,5 +58,5 @@ func main(){
                 }
             }
         }
-    }    
+    }
 }
